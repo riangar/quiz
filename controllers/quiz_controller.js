9f3059c3a2558391e68e9a21f88f8e1fc4229430
riangar <riandrade@gmail.com>
2015-07-27 00:46:09 +0200

    Búsqueda de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,7 +14,10 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes) {
+	var search = req.query.search || '';
+	search = search.replace(/ /g, '%');
+	search = '%' + search + '%';
+	models.Quiz.findAll({where: ['pregunta like ?', search]}).then(function(quizes) {
 		res.render('quizes/index', { quizes: quizes});
 	});
 };
