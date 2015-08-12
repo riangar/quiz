4d7cc3b1dd19bd8d7074543e6cd839934c7ca0b7
riangar <riandrade@gmail.com>
2015-08-12 17:35:56 +0200

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,4 +1,11 @@
-// Get /login   -- Formulario de login
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
     req.session.errors = {};
